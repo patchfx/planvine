44686e575f49ee7631b33a031d650816520655d6
Richard Patching <richard@justaddpixels.com>
2013-04-26 11:29:17 +0000

    Hash uses strings not symbols

--- lib/planvine/venue.rb
+++ lib/planvine/venue.rb
@@ -1,32 +1,33 @@
 module Planvine
   class Venue
     attr_reader :params
+
     def initialize(params)
       @params = params
     end
 
     def uid
-      @params.fetch(:uid, '')
+      @params.fetch("uid", '')
     end
 
     def name
-      @params.fetch(:name, '')
+      @params.fetch("name", '')
     end
 
     def lat
-      @params.fetch(:lat, '')
+      @params.fetch("lat", '')
     end
 
     def lng
-      @params.fetch(:lng, '')
+      @params.fetch("lng", '')
     end
 
     def address
-      @params.fetch(:address, '')
+      @params.fetch("address", '')
     end
 
     def short_address
-      @params.fetch(:short_address, '')
+      @params.fetch("short_address", '')
     end
   end
 end
